9123f28a34b23a0517e1dee40fdec7139583b6de
Philzen <philzen@riseup.net>
2021-12-27 01:48:01 +0100

    Add virtual piano stage 5/5 solution

--- 5 - Frontend Dev/101 - Virtual Piano/index.js
+++ 5 - Frontend Dev/101 - Virtual Piano/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("keydown", (e) => {
    const pressedKey = e.key.toUpperCase();
-   if ("ASDFGHJ".includes(pressedKey)) {
+   if ("ASDFGHJWETYU".includes(pressedKey)) {
       new Audio(`sound/${pressedKey}.mp3`).play();
    } else {
       console.warn(`There's no music behind the ${pressedKey} key.`)
