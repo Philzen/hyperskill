57c863e8d3485a725832745421336061b960621c
Philzen <philzen@riseup.net>
2022-02-05 18:46:55 +0100

    Add mineweeper solution 2/5 (unstyled, bare minimum to pass)

--- 5 - Frontend Dev/108 - Minesweeper/App.js
+++ 5 - Frontend Dev/108 - Minesweeper/App.js
@@ -2,12 +2,55 @@ import React from 'react';
 import loadingAnimationGif from './img/bomb.svg';
 import './App.css';
 
+let Cell = ({cellState}) => (
+  <td className="cell"></td>
+)
+
+let Row = ({columns}) => {
+    let cells = columns.map((cellState, columnIndex) =>
+      <Cell key={columnIndex} cellState={cellState} />
+    );
+    return <tr>{cells}</tr>
+}
+
+function PlayingField({mineMap}) {
+    let content = mineMap.map((row, rowIndex) =>
+      <Row key={rowIndex} columns={row} />
+    );
+    return (
+      <table className="playing-field">
+        <tbody>{content}</tbody>
+      </table>
+    );
+}
+
+let ControlPanel = () => (
+  <div>
+    <div className="flag-counter">10</div>
+    <button type="reset" />
+    <div className="timer">0:00</div>
+  </div>
+);
+
 function App() {
+  let mineMap = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={loadingAnimationGif} className="App-logo" alt="logo" />
-        <span>Minesweeper is loading...</span>
+        <ControlPanel />
+        <PlayingField mineMap={mineMap} />
       </header>
     </div>
   );
